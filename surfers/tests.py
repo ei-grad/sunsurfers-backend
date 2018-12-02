4466625b85113db2a82f5a216c4e24b2c8ad9342
Andrew Grigorev <andrew@ei-grad.ru>
2018-12-03 01:36:12 +0300

    Update sunmap

--- surfers/tests.py
+++ surfers/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+import logging
 
-# Create your tests here.
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class TestViews(TestCase):
+
+    def setUp(self):
+        get_user_model().objects.create_user(username='user', password='test')
+
+    def test_map_denies_anonymous(self):
+        response = self.client.get('/map/')
+        self.assertRedirects(response, '/login/?next=/map/')
+
+    def test_map_loads(self):
+        self.client.login(username='user', password='test')  # defined in fixture or with factory in setUp()
+        response = self.client.get('/map/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'sunmap.html')
--- surfers/urls.py
+++ surfers/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from surfers import views
 
 
+app_name = 'surfers'
 urlpatterns = [
     path('', views.sunmap, name='sunmap'),
     path('latest', views.latest, name='latest_geojson'),
